f0ff4e42040a8ebd6c626b88841a7a6396725c88
SebasTyler <ibikatti@gmail.com>
2020-08-18 12:04:24 -0400

    Arreglo de SEO y creacion sesión de cursos

--- dist/assets/js/index.js
+++ dist/assets/js/index.js
@@ -1,13 +1,19 @@
 const ipad = window.matchMedia('screen and (max-width: 768px)');
+const noIpad = window.matchMedia('screen and (min-width: 576px)');
 const menuMb = document.querySelector('.m-itemContent');
+const arrowButton = document.querySelector('#arrowNav');
 const burgerButton = document.querySelector('#burger');
 const burgerOut = document.querySelector('#burgerOut');
 const boxShadow = document.querySelector('.o-navFixedContainer');
 const bookmark = document.querySelector('#upBookmark');
 
-ipad.addListener(validation)
+const itemNav = document.querySelector('.m-menuCourse__folded');
+const shadowNavPlace = document.querySelector('.m-menuCourse__unfolded');
+const shadowNav = document.querySelector('.-shadow');
 
-function validation(event) {
+ipad.addListener(validationMd)
+
+function validationMd(event) {
     if (event.matches) {
         burgerButton.addEventListener('click', hideShow)
         burgerOut.addEventListener('click', hideShow)
@@ -18,7 +24,7 @@ function validation(event) {
     }
 }
 
-validation(ipad);
+validationMd(ipad);
 
 function hideShow() {
     if (menuMb.classList.contains('-isActive')) {
@@ -39,3 +45,26 @@ $(window).scroll(() => {
         $(".o-navFixedContainer").addClass(`a-boxShadowMenu`);
     }
   });
+
+// Class menu
+function navHideShow() {
+    if (itemNav.classList.contains('-isActive')) {
+        itemNav.classList.remove('-isActive');
+        shadowNavPlace.classList.remove('-shadow');
+    } else {
+        itemNav.classList.add('-isActive');
+        shadowNavPlace.classList.add('-shadow');
+    }
+}
+  noIpad.addListener(validationnoMd)
+
+function validationnoMd(event) {
+      if (event.matches) {
+          arrowButton.addEventListener('click', navHideShow)
+      }
+      else {
+          arrowButton.removeEventListener('click', navHideShow)
+      }
+  }
+  
+  validationnoMd(noIpad);
